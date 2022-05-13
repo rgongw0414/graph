80641cd581f63e6ddf397580a3c0c18dcdfebc52
rgongw <a1055547@mail.nuk.edu.tw>
2022-05-13 22:31:30 +0800

    use emplace to avoid implicit creation of temporary element

--- graph.cpp
+++ graph.cpp
@@ -1,47 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+static int gid = 0;
 class Node{
     public:
     int id;
     int state;
     int value;
-    Node():id(gid++), state(0), value(0){};
-    Node(int v):id(gid++), state(0), value(v){};
-    friend operator==(Node a, Node b){
-        return a.state == b.state && a.value == b.value;
+    Node():id(gid++), state(0), value(0){}
+    Node(int v):id(gid++), state(0), value(v){}
+    friend bool operator==(Node a, Node b){
+        return a.id == b.id && a.state == b.state && a.value == b.value;
     }
 };
-static int gid = 0;
 
 class Graph{
     public:
-    vector<
-        pair<Node, list<int>>
-    > graph;
-    // vector<list<int>> graph;
+    map<
+        int, pair<Node, list<Node>>
+    > LIST;
+    Graph(){}
     void insertNode(int value);
-    void connect(Node a, Node b); // create an edge between a & b
-    void disconnect(Node a, Node b); // delete the edge between a & b
+    void insertNode(Node &node);
+    void connect(Node &a, Node &b); // create an edge between a & b
+    void disconnect(Node &a, Node &b); // delete the edge between a & b
 };
 
-Graph::insertNode(int value){
+void Graph::insertNode(int value){
     Node node = Node(value);
-    list<int> List;
-    graph.push_back(make_pair(node, List));
+    list<Node> l;
+    // instead of "insert", use "emplace", it avoids implicit creation of temporary element w.r.t. container.
+    // "emplace" use the parameters to create element
+    LIST.emplace(node.id, make_pair(node, l));  // i.e. use node.id, make_pair(node, l) to create element in map
 }
 
-Graph::connect(Node a, Node b){
-    //find(graph.begin(), graph.end(), a)
+void Graph::insertNode(Node &node){
+    list<Node> l;
+    LIST.emplace(node.id, make_pair(node, l));
+}
+
+void Graph::connect(Node &a, Node &b){
+    //find(LIST.begin(), LIST.end(), a)
+    LIST[a.id].push_back(b);
+    LIST[b.id].push_back(a);
 }
 
 int main(){
     Node a = Node(1);
-    Node b = Node(2);
-    int c = (a == b);
-    cout << c << endl;
-    // m[a] = a.value;
-    // m[b] = b.value;
-    // cout << m[a] << endl;
-    // cout << m[b] << endl;
+    Graph g = Graph();
+    g.insertNode(a);
+    cout << g.LIST[0].first.id << endl;
+    g.insertNode(2);
+    cout << g.LIST[1].first.id << endl;
 }
